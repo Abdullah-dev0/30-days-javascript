2e0645980dca9f4f679fd10dda22d023468fed9f
Abdullah-142 <abdulah14200@gmail.com>
2024-07-21 16:18:20 +0500

    Complete Day-9 both in javaacript and typescript event handling remove and add class much more

---
+++ Day-9/Typescript/dist/day-8-typescript.js
@@ -0,0 +1,66 @@
+"use strict";
+// Es6 features
+// --------------------- Activity 1: Template literals
+// Task 1 : Use template literals to Create a String that includes variables for a person's name, age. Then log the string to the console.
+let Name = "Abdullah";
+let age = 23;
+const intro = `Helo my name is ${Name} and my age is ${age} and i Am a full Stack Developer`;
+console.log(intro);
+// Task 2 : Create a multi line string using template literals. Then log the string to the console.
+console.log("------------Printing MultiLineSting-----------");
+const multiLineSting = `Helo Guyz 
+my name is Abdullah
+and my age is 23 
+and i am a full stack developer
+`;
+console.log(multiLineSting);
+// ---------------- Activity 2: Destructuring
+// Task 3 : Use Array destructing to extract the first and second value from the array of numbers  and log to the console.
+const array = [1, 23, 3, 4, 5, 5];
+const [first, second] = array;
+console.log("first number of array = " + first + " Second number if array = " + second);
+const book = {
+    title: "The Art of being alone",
+    author: "Abdullah",
+};
+const { title, author } = book;
+console.log("The title of book : " + title + "    " + "The author of book : " + author);
+// ---------------------------- Activity 3: Spread Operator and Rest operators
+// Task 5 : Use the spread operator to create new array that includes all element of an existing array and add an additional element to the new array. Then log the new array to the console.
+const array2 = [1, 2, 3];
+const newArray = [...array2, 4, 5, 6, 7];
+console.log(newArray);
+// Task 6 : Use the rest operator to create a function that accpect an arbitrary number of arguments , sum them and return the result.
+const sum = (...args) => {
+    return args.reduce((a, b) => a + b, 0);
+};
+console.log("Sum of numbers = " + sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+// --------------------- Activity 4: Default parameters
+// Task 7 : Create a function that accepts two parameters, and return there product , with the second parameter default to 1. Log the result if calling this function with and without the second parameter.
+function product(num1, num2 = 1) {
+    return num1 * num2;
+}
+console.log("Product with both parameters = ", product(2, 22));
+function product2(num1, num2 = 1) {
+    return num1 * num2;
+}
+console.log("Product wihtout 2 parameter = ", product2(19));
+const obj = {
+    name: "Abdullah",
+    age: 23,
+    profession: "Full Stack Developer",
+    getInfo() {
+        return `My name is ${this.name} and my age is ${this.age} and i am a ${this.profession}`;
+    },
+};
+console.log(obj);
+const key = "name";
+const key2 = "age";
+const key3 = "profession";
+const obj2 = {
+    [key]: "Abdullah",
+    [key2]: 23,
+    [key3]: "Full Stack Developer",
+};
+console.log(obj2);
+console.log("Keys of Objects = ", Object.keys(obj2));
---
+++ Day-9/Typescript/script.ts
@@ -0,0 +1,87 @@
+// Activity 1 : Dom Manipulation
+
+// Task 1 : Select an Html element by its id and change its text content.
+
+const changeContent: HTMLElement | null = document.getElementById("task-1");
+
+changeContent!.innerHTML = "Hey my name is Jhon Doe";
+
+// Task 2 : Select an Html element by its class and change its background color.
+
+const changeBackgroundColour = document.getElementsByClassName("task-2") as HTMLCollectionOf<HTMLElement>;
+
+changeBackgroundColour[0].style.backgroundColor = "red";
+
+// -------------- or you can also use querySelector --------
+
+const changeBackgroundColour2: HTMLInputElement | null = document.querySelector(".task-2");
+
+changeBackgroundColour2!.style.backgroundColor = "yellow";
+
+// ----------------------- Activity 2 : Creating and appending elements
+
+// Task 3 : Create a new div element with some content and append it to the body of the html document.
+
+const newDiv: HTMLDivElement = document.createElement("div");
+
+newDiv.innerHTML = "This is a new div element";
+
+document.body.appendChild(newDiv);
+
+// Task 4 : Create a new li element and add it to exting ul list".
+
+const newList: HTMLElement = document.createElement("li");
+
+const getUl: HTMLElement | null = document.getElementById("task-4");
+
+newList.innerHTML = "This is a new list item";
+
+getUl!.appendChild(newList);
+
+// Activity 3 : Removing elements
+
+// Task 5 : Select an element and remove it from Dom.
+const removeElement: HTMLElement | null = document.getElementById("task-5");
+
+removeElement!.remove();
+
+// Task 6: Remove the last child of a specific HTMl Element.
+
+const removeLastChild: HTMLElement | null = document.getElementById("task-4");
+
+removeLastChild!.lastElementChild!.remove();
+
+// ------------------------- Acticity 4 : Modifying Attributes and classs
+
+// Task 7 : Select an element and chnage one its attribute (e.g . src to img tag).
+
+const changeAttribute = document.getElementById("task-7") as HTMLImageElement;
+
+changeAttribute.src = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
+
+// Task 8 : Add and remove a css class to/from and html element .
+
+const addAndRemoveClass: HTMLElement | null = document.getElementById("task-8");
+
+addAndRemoveClass!.classList.remove("task-8");
+
+addAndRemoveClass!.classList.add("task-8-2");
+
+// ------------------------- Activity 5 : Event Handling
+
+// Task 9 : Add an  click event listener to a button that change the text content of  a paragraph.
+
+const getButtonById = document.getElementById("task-9") as HTMLButtonElement;
+const getContent: HTMLElement | null = document.getElementById("contentToChange");
+
+getButtonById!.addEventListener("click", (event: MouseEvent) => {
+	getContent!.innerHTML = "Content changed";
+});
+
+// Task 10: add a mouseover event listener to an element that change its border color.
+
+const changeBroderColor: HTMLElement | null = document.getElementById("task-10");
+
+changeBroderColor!.addEventListener("mouseover", function (event: MouseEvent) {
+	changeBroderColor!.style.border = "2px solid red";
+});
